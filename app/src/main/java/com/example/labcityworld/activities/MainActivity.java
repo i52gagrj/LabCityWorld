3cf6615cb80403a8293c4057cff5f0b1b1853711
i52gagrj <i52gagrj@gmail.com>
2023-01-09 13:34:18 +0100

    202301091334

--- app/src/main/java/com/example/labcityworld/activities/MainActivity.java
+++ app/src/main/java/com/example/labcityworld/activities/MainActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.labcityworld.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab = (FloatingActionButton) findViewById(R.id.fabAddCity);
+        
     }
 }
